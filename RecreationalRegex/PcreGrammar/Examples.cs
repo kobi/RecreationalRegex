3f56d0169b02de30d0ad118f077a7dd6bfd915d8
Kobi <kobikobi@gmail.com>
2016-09-20 17:26:03 +0200

    Upgrade to Nunit 3.4.1.

--- RecreationalRegex/PcreGrammar/Examples.cs
+++ RecreationalRegex/PcreGrammar/Examples.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 
 namespace Kobi.RecreationalRegex.PcreGrammar
 {
     public class Examples
     {
-        public readonly IEnumerable<string> Valid = new[]{
+        public static readonly IEnumerable<string> Valid = new[]{
 "abcdefg","abc,def,ghi","abc,,,def",",,,,,,","[abc;]","[a,bc;]","sss[abc;d]","as[abc;d,e]","[abc;d,e][fgh;j,k]",
 "<abc>","[<a>b;<c,d>,<e,f>]","<a,b,c>","<a,bb,c>","<,,,>","<>","<><>","<>,<>","a<<<<>>><a>>","<<<<<>>>><><<<>>>>",
 "<z>[a;b]","<z[a;b]>","[[;];]","[,;,]","[;[;]]","[<[;]>;<[;][;,<[;,]>]>]",
         };
 
-        public readonly IEnumerable<string> Invalid = new[]{
+        public static readonly IEnumerable<string> Invalid = new[]{
 "<a","bc>","<abc<de>","[a<b;c>;d,e]","[a]","<<<<<>>>><><<<>>>>>","<<<<<>>>><><<<>>>","[abc;def;]","[[;],]","[;,,]","[abc;d,e,f]",
 "[<[;]>;<[;][;,<[;,]>]]>","<z[a;b>]",
     };
